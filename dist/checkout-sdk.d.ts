cfd6100f78dd5d56d8adf2c201123bba467393ee
FranciscoOnt <fontiveros@intersysconsulting.com>
2021-07-06 18:44:05 -0500

    sample

--- dist/checkout-sdk.d.ts
+++ dist/checkout-sdk.d.ts
@@ -4504,20 +4504,84 @@ declare interface MolliePaymentInitializeOptions {
     form?: HostedFormOptions;
 }
 
-declare interface MonerisStylingProps {
-    cssBody?: string;
-    cssTextbox?: string;
-    cssTextboxPan?: string;
-    cssTextboxExpiry?: string;
-    csstexboxCvd?: string;
-}
-
-declare interface MonerisaymentInitializeOptions {
+/**
+ * A set of options that are required to initialize the Moneris payment method.
+ *
+ * Once Moneris payment is initialized, a credit card payment form is provided by the
+ * payment provider as an IFrame, it will be inserted into the current page. These
+ * options provide a location and styling for the payment form.
+ *
+ * ```js
+ * service.initializePayment({
+ *      methodId: 'moneris',
+ *      moneris: {
+ *          containerId: 'container',
+ *          style : {
+ *              cssBody: 'background:white;';
+ *              cssTextbox: 'border-width:2px;';
+ *              cssTextboxPan: 'width:140px;';
+ *              cssTextboxExpiry: 'width:40px;';
+ *              csstexboxCvd: 'width:40px';
+ *          }
+ *      }
+ * });
+ * ```
+ */
+declare interface MonerisPaymentInitializeOptions {
     /**
      * The ID of a container where the Moneris iframe component should be mounted
      */
     containerId: string;
+    /**
+     * The styling props to apply to the iframe component
+     */
     style?: MonerisStylingProps;
+    /**
+     * Hosted Form Validation Options
+     */
+    form?: HostedFormOptions;
+}
+
+/**
+ * A set of stringified CSS to apply to Moneris' IFrame fields.
+ * CSS attributes should be converted to string.
+ * Please note that ClassNames are not supported.
+ *
+ * IE:
+ * ```js
+ * {
+ *      cssBody: 'background:white;';
+ *      cssTextbox: 'border-width:2px;';
+ *      cssTextboxPan: 'width:140px;';
+ *      cssTextboxExpiry: 'width:40px;';
+ *      csstexboxCvd: 'width:40px;';
+ * }
+ * ```
+ *
+ * When using several attributes use semicolon to separate each one.
+ * IE: 'background:white;width:40px;'
+ */
+declare interface MonerisStylingProps {
+    /**
+     * Stringified CSS to apply to the body of the IFrame.
+     */
+    cssBody?: string;
+    /**
+     * Stringified CSS to apply to each of input fields.
+     */
+    cssTextbox?: string;
+    /**
+     * Stringified CSS to apply to the card's number field.
+     */
+    cssTextboxPan?: string;
+    /**
+     * Stringified CSS to apply to the card's expiry field.
+     */
+    cssTextboxExpiry?: string;
+    /**
+     * Stringified CSS to apply to the card's CVV field.
+     */
+    cssTexboxCvd?: string;
 }
 
 declare interface NonceGenerationError {
@@ -4733,7 +4797,7 @@ declare interface PaymentInitializeOptions extends PaymentRequestOptions {
      * The options that are required to initialize the Moneris payment method.
      * They can be omitted unless you need to support Moneris.
      */
-    moneris?: MonerisaymentInitializeOptions;
+    moneris?: MonerisPaymentInitializeOptions;
     /**
      * The options that are required to initialize the PayPal Express payment method.
      * They can be omitted unless you need to support PayPal Express.
--- docs/interfaces/monerisstylingprops.md
+++ docs/interfaces/monerisstylingprops.md
@@ -2,6 +2,24 @@
 
 # Interface: MonerisStylingProps
 
+A set of stringified CSS to apply to Moneris' IFrame fields.
+CSS attributes should be converted to string.
+Please note that ClassNames are not supported.
+
+IE:
+```js
+{
+     cssBody: 'background:white;';
+     cssTextbox: 'border-width:2px;';
+     cssTextboxPan: 'width:140px;';
+     cssTextboxExpiry: 'width:40px;';
+     csstexboxCvd: 'width:40px;';
+}
+```
+
+When using several attributes use semicolon to separate each one.
+IE: 'background:white;width:40px;'
+
 ## Hierarchy
 
 * **MonerisStylingProps**
@@ -11,10 +29,10 @@
 ### Properties
 
 * [cssBody](monerisstylingprops.md#optional-cssbody)
+* [cssTexboxCvd](monerisstylingprops.md#optional-csstexboxcvd)
 * [cssTextbox](monerisstylingprops.md#optional-csstextbox)
 * [cssTextboxExpiry](monerisstylingprops.md#optional-csstextboxexpiry)
 * [cssTextboxPan](monerisstylingprops.md#optional-csstextboxpan)
-* [csstexboxCvd](monerisstylingprops.md#optional-csstexboxcvd)
 
 ## Properties
 
@@ -22,11 +40,23 @@
 
 • **cssBody**? : *undefined | string*
 
+Stringified CSS to apply to the body of the IFrame.
+
+___
+
+### `Optional` cssTexboxCvd
+
+• **cssTexboxCvd**? : *undefined | string*
+
+Stringified CSS to apply to the card's CVV field.
+
 ___
 
 ### `Optional` cssTextbox
 
 • **cssTextbox**? : *undefined | string*
+
+Stringified CSS to apply to each of input fields.
 
 ___
 
@@ -34,14 +64,12 @@ ___
 
 • **cssTextboxExpiry**? : *undefined | string*
 
+Stringified CSS to apply to the card's expiry field.
+
 ___
 
 ### `Optional` cssTextboxPan
 
 • **cssTextboxPan**? : *undefined | string*
 
-___
-
-### `Optional` csstexboxCvd
-
-• **csstexboxCvd**? : *undefined | string*
+Stringified CSS to apply to the card's number field.
